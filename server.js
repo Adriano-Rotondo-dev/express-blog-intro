c0d691d5ce66f027d48f1f97ae75a1f2e33ce85e
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-27 17:54:47 +0200

    Refactor server.js to implement /api/posts route and populate foodArray with detailed content

--- server.js
+++ server.js
@@ -1,55 +1,57 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
-//TODO: rimuovi il commento per gli asset statici da visualizzare
-// app.use(express.static('public'))
+app.use(express.static('public'))
 app.listen(port, () => {
   console.log(`Your server is running on http://localhost:${port}`);
 });
 
-app.get('/', (req, res) => {
-  res.send('Server del mio blog');
+app.get("/", (req, res) => {
+  res.send("Server del mio blog");
 });
-//*creo array con 5 post di cui delineo il contenuto ->riferimenti in reaadme
-//TODO: RIEMPI I CAMPI
-const foodArray = [
-  {
-    titolo: "Ciambellone",
-    contenuto: "",
-    immagine: `http://localhost:${port}/images/ciambellone.jpeg`,
-    tags: ["dolce", "colazione", "tradizionale", "soffice", "uova"],
-  },
-  {
-    titolo: "Cracker alla barbabietola",
-    contenuto:
-      "Un dolce soffice e genuino, perfetto per la colazione o la merenda.",
-    immagine: `http://localhost:${port}/images/cracker_barbabietola.jpeg`,
-    tags: ["salato", "snack", "barbabietola", "vegetariano"],
-  },
-  {
-    titolo: "Pane fritto dolce",
-    contenuto: "Cracker croccanti e colorati, ideali come snack salutare.",
-    immagine: `http://localhost:${port}/images/pane_fritto_dolce.jpeg`,
-    tags: ["dolce", "fritto", "merenda", "pane"],
-  },
-  {
-    titolo: "Pasta alla barbabietola",
-    contenuto:
-      "Fette di pane immerse nell'uovo e fritte, spolverate con zucchero.",
-    immagine: `http://localhost:${port}/images/pasta_barbabietola.jpeg`,
-    tags: ["primo", "barbabietola", "vegetariano", "colorato"],
-  },
-  {
-    titolo: "Torta Paesana",
-    contenuto:
-      "Pasta dal colore vivace grazie alla barbabietola, gustosa e originale.",
-    immagine: `http://localhost:${port}/images/torta_paesana.jpeg`,
-    tags: ["dolce", "tradizionale", "pane", "cacao"],
-  },
-];
 
-//TODO:Crea rotto /api/posts per restituire oggetto json con lista dei post
+//TODO:Crea route /api/posts per restituire oggetto json con lista dei post 🤔
+//*creo array con 5 post di cui delineo il contenuto ->riferimenti in readme
 
-//*configura gli asset statici [vedi sopra] per assicurartene la visualizzazione in postman
+app.get("/api/posts", (req, res) => {
+  const foodArray = [
+    {
+      titolo: "Ciambellone",
+      contenuto: "Un dolce tradizionale, soffice e gustoso.",
+      immagine: `http://localhost:${port}/images/ciambellone.jpeg`,
+      tags: ["dolce", "colazione", "tradizionale", "soffice", "uova"],
+    },
+    {
+      titolo: "Cracker alla barbabietola",
+      contenuto:
+        "Un dolce soffice e genuino, perfetto per la colazione o la merenda.",
+      immagine: `http://localhost:${port}/images/cracker_barbabietola.jpeg`,
+      tags: ["salato", "snack", "barbabietola", "vegetariano"],
+    },
+    {
+      titolo: "Pane fritto dolce",
+      contenuto: "Cracker croccanti e colorati, ideali come snack salutare.",
+      immagine: `http://localhost:${port}/images/pane_fritto_dolce.jpeg`,
+      tags: ["dolce", "fritto", "merenda", "pane"],
+    },
+    {
+      titolo: "Pasta alla barbabietola",
+      contenuto:
+        "Fette di pane immerse nell'uovo e fritte, spolverate con zucchero.",
+      immagine: `http://localhost:${port}/images/pasta_barbabietola.jpeg`,
+      tags: ["primo", "barbabietola", "vegetariano", "colorato"],
+    },
+    {
+      titolo: "Torta Paesana",
+      contenuto:
+        "Pasta dal colore vivace grazie alla barbabietola, gustosa e originale.",
+      immagine: `http://localhost:${port}/images/torta_paesana.jpeg`,
+      tags: ["dolce", "tradizionale", "pane", "cacao"],
+    },
+  ];
+  res.json(foodArray);
+});
 
-//TODO: testa il tuo applicativo
+//*configura gli asset statici [vedi sopra] per assicurartene la visualizzazione in postman ✔
+
+//*testa il tuo applicativo su postman ✔
